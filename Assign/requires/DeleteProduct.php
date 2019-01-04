db3efe912417fc588662ee7666dd191e0c14be3b
Luke Elam <41964673+dstlny@users.noreply.github.com>
2019-01-04 17:18:33 +0000

    Update DeleteProduct.php

--- Assign/requires/DeleteProduct.php
+++ Assign/requires/DeleteProduct.php
@@ -1,15 +1,8 @@
 <?php
-//Make connection to database
+//real simple page.
 include '../pages/dbcon/init.php';
-
-//Gather id from $_GET[]
 $id=$_GET['id'];
-
 $query="DELETE FROM webProducts WHERE ProductID='$id'";
-//Construct DELETE query to remove record where WHERE id provided equals the id in the table
-
-//run $query
 mysqli_query($connection, $query);
-
 (mysqli_affected_rows($connection) > 0) ? header("location: adminPanel.php") : header("location: adminPanel.php");		
 ?>
